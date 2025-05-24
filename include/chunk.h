8a0fcaa06577006c01900780d23fbf3c75b609e0
Andrés <nunezlopez.ja@gmail.com>
2025-05-24 12:39:50 -0600

    Cleaned shit a lil

--- include/chunk.h
+++ include/chunk.h
@@ -6,18 +6,9 @@
 typedef struct {
     uint32_t length;
     CHUNK_TYPE chunk_type;
-    /* uint8_t type[4]; */
-    /* uint8_t data[4]; */
     uint8_t *data;
-    /* uint8_t crc[4]; */
     uint32_t crc;
 } __attribute__((__packed__)) CHUNK;
-
-// Try to "populate" a chunk?
-/* CHUNK get_chunk() { */
-/*     uint32_t length = 0; */
-/*     CHUNK_TYPE chunk_t; */
-/* } */
 
 // Helper to swap endianness
 uint32_t swap_endian(uint32_t val);
